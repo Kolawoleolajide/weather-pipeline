fdb0ed68182a82013fed14544b9967ffd13e413b
Kolawole Olajide <kolawole.olajide.e@gmail.com>
2026-04-03 00:20:36 +0100

    Add files via upload

---
+++ weather_pipeline.py
@@ -0,0 +1,39 @@
+import requests
+import psycopg2
+from datetime import datetime
+
+API_KEY = "bdf17317ccaa08f29bfabb76881e56c9"
+
+url = f"https://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=bdf17317ccaa08f29bfabb76881e56c9&units=metric"
+
+response = requests.get(url)
+data = response.json()
+
+record = (
+    datetime.now(),
+    "Lagos",
+    data["main"]["temp"],
+    data["main"]["humidity"],
+    data["wind"]["speed"],
+    data["weather"][0]["description"]
+)
+
+conn = psycopg2.connect(
+    "postgresql://postgres.byntkxqnjshttsyrgczo:Ball2347962%25@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?sslmode=require"
+)
+
+
+cur = conn.cursor()
+
+query = """
+INSERT INTO weather_data (timestamp, city, temperature, humidity, windspeed, condition)
+VALUES (%s, %s, %s, %s, %s, %s)
+"""
+
+cur.execute(query, record)
+
+conn.commit()
+cur.close()
+conn.close()
+
+print("Data inserted")
